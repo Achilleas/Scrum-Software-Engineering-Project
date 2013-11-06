ef2e997db187839a207583b7382a8059e1a27825
calum0607 <calum0607@hotmail.co.uk>
2013-11-06 09:28:37 +0000

    add  new print

--- src/TestClass.java
+++ src/TestClass.java
@@ -4,6 +4,7 @@ public class TestClass {
 	public static void main(String[] args) {
 		System.out.println("THIS IS A TEST!!!!!!!!!");
 		System.out.println("THIS IS ANOTHER TEST");
+		System.out.println("THIS IS ANOTHER TEST");
 	}
 
 }
