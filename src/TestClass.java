0799f464f8b422eec9ae843a8fd9725eacc83d38
Qiao Kang <qk@st-andrews.ac.uk>
2013-11-06 09:41:38 +0000

    Qiao

--- src/TestClass.java
+++ src/TestClass.java
@@ -3,6 +3,7 @@ public class TestClass {
 
 	public static void main(String[] args) {
 		System.out.println("if this doesn't work....");
+		System.out.println("if this doesn't work....");
 		System.out.println("THIS IS A TEST!!!!!!!!!");
 		System.out.println("THIS IS ANOTHER TEST");
 		System.out.println("THIS IS ANOTHER TEST");
