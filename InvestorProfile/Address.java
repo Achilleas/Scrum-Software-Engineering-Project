44cd83b3ff70037a9747bc49626d3bf9c157cfd6
CWKenny <cwk4@st-andrews.ac.uk>
2013-11-07 15:28:16 +0000

    getAddress

--- InvestorProfile/Address.java
+++ InvestorProfile/Address.java
@@ -7,6 +7,7 @@ public class Address {
 	private String postcode;
 	private String country;
 	
+	//Constructor
 	public Address(String line1, String line2, String city, String county, String postcode, String country){
 		this.line1 = line1;
 		this.line2 = line2;
@@ -14,6 +15,18 @@ public class Address {
 		this.county = county;
 		this.postcode = postcode;
 		this.country = country;
+	}
+	
+	//Returns String Array containing Address elements
+	public String[] getAddress() {
+		String add[6];
+		add[0] = line1;
+		add[1] = line2;
+		add[2] = city;
+		add[3] = county;
+		add[4] = postcode;
+		add[5] = country;
+		return add;
 	}
 	
 }
