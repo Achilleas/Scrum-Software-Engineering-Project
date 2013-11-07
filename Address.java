a4d44ce23099457d98a0949cc3cf9975e6dc9abb
CWKenny <cwk4@st-andrews.ac.uk>
2013-11-07 15:08:24 +0000

    Address Class

---
+++ Address.java
@@ -0,0 +1,19 @@
+//Home Address Class
+public class Address {
+	private String line1;
+	private String line2;
+	private String city;
+	private String county;
+	private String postcode;
+	private String country;
+	
+	public Address(String line1, String line2, String city, String county, String postcode, String country){
+		this.line1 = line1;
+		this.line2 = line2;
+		this.city = city;
+		this.county = county;
+		this.postcode = postcode;
+		this.country = country;
+	}
+	
+}
