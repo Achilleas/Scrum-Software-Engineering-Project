14f78a11e22cd9572453c17c77451103c554a137
Jia Heng <jhe3@st-andrews.ac.uk>
2013-11-08 15:58:56 +0000

    edit contructor and add getter

--- src/main/InvestorProfile.java
+++ src/main/InvestorProfile.java
@@ -2,16 +2,39 @@ package main;
 
 import java.util.ArrayList;
 
+import org.apache.commons.lang3.Validate;
+
 
 public class InvestorProfile {
 	
-	String firstName;
-	String surname;
-	ArrayList companiesInvested;
-	ArrayList companiesInterested;
-	String username;
-	String password;
+	private String firstName;
+	private String surname;
+	private ArrayList companiesInvested;
+	private ArrayList companiesInterested;
+	private String username;
+	private String password;
 	
-	public InvestorProfile(String firstName, String surname, ArrayList companiesInvested, ArrayList companiesInterested){}
+	public InvestorProfile(String firstName, String surname, ArrayList companiesInvested, ArrayList companiesInterested) {
+		
+		Validate.notNull(firstName, "firstName can't be null");
+		Validate.notNull(surname, "surname can't be null");
+		//Validate.notNull(username, "username can't be null");
+		//Validate.notNull(password, "password can't be null");
+		
+		this.firstName = firstName;
+		this.surname = surname;
+		this.companiesInterested = companiesInterested;
+		this.companiesInvested = companiesInvested;
+		//this.username = username;
+		//this.password = password;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public String getSurname() {
+		return surname;
+	}
 	
 }
