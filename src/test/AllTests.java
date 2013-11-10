c3b2fe00fbf0f7814fdc97730b3f007727a36b6a
Jia Heng <jhe3@st-andrews.ac.uk>
2013-11-10 14:00:23 +0000

    change class name

--- src/test/AllTests.java
+++ src/test/AllTests.java
@@ -6,7 +6,7 @@ import org.junit.runners.Suite.SuiteClasses;
 
 @RunWith(Suite.class)
 @SuiteClasses({ 
-	TestFinanceQuery.class,
+	FinanceQueryTest.class,
 	AnalyzerTest.class
 })
 
--- src/test/FinanceQueryTest.java
+++ src/test/FinanceQueryTest.java
@@ -15,7 +15,7 @@ import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
-public class TestFinanceQuery {
+public class FinanceQueryTest {
 	
 	Date fromDate, toDate;
 	
