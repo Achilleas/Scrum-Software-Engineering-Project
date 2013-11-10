123fb21b7371b4e14692f3fd69be697bbed8b90b
CWKenny <cwk4@st-andrews.ac.uk>
2013-11-10 23:56:45 +0000

    Test Update

--- src/main/Investor.java
+++ src/main/Investor.java
@@ -5,12 +5,11 @@ import java.util.Date;
 
 import org.apache.commons.lang3.Validate;
 
-
 public class Investor {
-	
+
 	private String username;
 	private String password;
-	private Date dob;
+	private DOB dob;
 	private String firstName;
 	private String surname;
 	private String email;
@@ -18,8 +17,8 @@ public class Investor {
 	private Address address;
 	private ArrayList<String> companiesInvested;
 	private ArrayList<String> companiesInterested;
-	
-	public Investor(String username, String password, Date dob,
+
+	public Investor(String username, String password, DOB dob,
 			String firstName, String surname, String email, String telephone,
 			Address address, ArrayList<String> companiesInvested,
 			ArrayList<String> companiesInterested) {
@@ -35,133 +34,156 @@ public class Investor {
 		this.companiesInterested = companiesInterested;
 	}
 
-	public Investor(String firstName2, String surname2, String username2,
-			String password2, ArrayList<String> companiesInvested2,
+	public Investor(String username2, String password2, String firstName2,
+			String surname2, ArrayList<String> companiesInvested2,
 			ArrayList<String> companiesInterested2) {
 		// TODO Auto-generated constructor stub
 	}
 
-	public Investor(String firstName2, String surname2, String username2,
-			String password2) {
+	public Investor(String username2, String password2, String firstName2, String surname2) {
 		// TODO Auto-generated constructor stub
 	}
 
-	public Investor(String firstName, String surname,
-			String password2) {
-		
+	public Investor(String firstName, String surname, String password2) {
+
 		Validate.notNull(firstName, "firstName can't be null");
 		Validate.notNull(surname, "surname can't be null");
-		//Validate.notNull(username, "username can't be null");
-		//Validate.notNull(password, "password can't be null");
-		
+		// Validate.notNull(username, "username can't be null");
+		// Validate.notNull(password, "password can't be null");
+
 		this.firstName = firstName;
 		this.surname = surname;
 		this.companiesInterested = new ArrayList<String>();
 		this.companiesInvested = new ArrayList<String>();
-		//this.username = username;
-		//this.password = password;
+		// this.username = username;
+		// this.password = password;
 	}
-	
-	
+
 	public String getUsername() {
 		return username;
 	}
+
 	public String getPassword() {
 		return password;
 	}
+
 	public String getFirstName() {
 		return firstName;
 	}
+
 	public String getSurname() {
 		return surname;
 	}
-	public Date getDob() {
+
+	public DOB getDob() {
 		return dob;
 	}
+
 	public String getEmail() {
 		return email;
 	}
+
 	public String getTelephone() {
 		return telephone;
 	}
+
 	public Address getAddress() {
 		return address;
 	}
+
 	public ArrayList<String> getCompaniesInvested() {
 		return companiesInvested;
 	}
+
 	public ArrayList<String> getCompaniesInterested() {
 		return companiesInterested;
 	}
-	
+
 	public void setUsername(String username) {
 		this.username = username;
 	}
+
 	public void setPassword(String password) {
 		this.password = password;
 	}
-	public void setDob(Date dob) {
+
+	public void setDob(DOB dob) {
 		this.dob = dob;
 	}
+
 	public void setFirstName(String firstName) {
 		this.firstName = firstName;
 	}
+
 	public void setSurname(String surname) {
 		this.surname = surname;
 	}
+
 	public void setEmail(String email) {
 		this.email = email;
 	}
+
 	public void setTelephone(String telephone) {
 		this.telephone = telephone;
 	}
+
 	public void setAddress(Address address) {
 		this.address = address;
 	}
 
 	/**
 	 * -------------------------------------------------------------------------
+	 * 
 	 * @author Qiao
-	 * -------------------------------------------------------------------------
+	 *         --------------------------------------------------------------
+	 *         -----------
 	 * @param password
 	 * @return if change is made
 	 */
-	public boolean verifyPassword(String password){
+	public boolean verifyPassword(String password) {
 		return password.equals(this.password);
 	}
-	public boolean changePassword(String old_password,String new_password){
-		if(verifyPassword(old_password)){
-			this.password=new_password;
+
+	public boolean changePassword(String old_password, String new_password) {
+		if (verifyPassword(old_password)) {
+			this.password = new_password;
 			return true;
-		}else{
+		} else {
 			return false;
 		}
 	}
-	public void addInterested(String stock){
-		if(!isStockList(stock,companiesInterested)){
+
+	public void addInterested(String stock) {
+		if (!isStockList(stock, companiesInterested)) {
 			companiesInterested.add(stock);
 		}
 	}
-	public void addInvested(String stock){
-		if(!isStockList(stock,companiesInvested)){
+
+	public void addInvested(String stock) {
+		if (!isStockList(stock, companiesInvested)) {
 			companiesInvested.add(stock);
 		}
 	}
-	public boolean isInterested(String stock){
-		return isStockList(stock,companiesInterested);
+
+	public boolean isInterested(String stock) {
+		return isStockList(stock, companiesInterested);
 	}
-	public boolean isInvested(String stock){
-		return isStockList(stock,companiesInvested);
+
+	public boolean isInvested(String stock) {
+		return isStockList(stock, companiesInvested);
 	}
-	public ArrayList<String> getInterestList(){
+
+	public ArrayList<String> getInterestList() {
 		return companiesInterested;
 	}
-	public ArrayList<String> getInvestedList(){
+
+	public ArrayList<String> getInvestedList() {
 		return companiesInterested;
 	}
-	private static boolean isStockList(String stock,ArrayList<String> list){
-		for(int i=0;i<list.size();i++){
-			if(stock.equals(list.get(i))){
+
+	private static boolean isStockList(String stock, ArrayList<String> list) {
+		for (int i = 0; i < list.size(); i++) {
+			if (stock.equals(list.get(i))) {
 				return true;
 			}
 		}
--- src/test/InvestorTest.java
+++ src/test/InvestorTest.java
@@ -7,6 +7,7 @@ import java.util.ArrayList;
 import java.util.Date;
 
 import main.Address;
+import main.DOB;
 import main.Investor;
 
 import org.junit.Before;
@@ -14,68 +15,90 @@ import org.junit.BeforeClass;
 import org.junit.Test;
 
 public class InvestorTest {
-	
+
 	String username = "john";
 	String password = "1234";
 	String firstName = "John";
 	String surname = "Doe";
-	Date dob = new Date();
+	DOB dob = new DOB(23, 11, 1950);
 	String email = "jd@hotmail.co.uk";
 	String telephone = "07754791234";
-	Address address = new Address("5 Hartwell House", "Strawberry Terrace", "Oz", "Wizardshire", "OZ15 9YR", "Oztopia");
+	Address address = new Address("5 Hartwell House", "Strawberry Terrace",
+			"Oz", "Wizardshire", "OZ15 9YR", "Oztopia");
 	ArrayList<String> companiesInvested;
 	ArrayList<String> companiesInterested;
-	
+
 	@Before
 	public void setupBefore() {
 		companiesInvested = new ArrayList<String>();
 		companiesInterested = new ArrayList<String>();
-		
+
 		companiesInterested.add("HSBA");
 		companiesInvested.add("BARC");
 	}
-	
+
 	@Test
 	public void getterTest() {
-		Investor john = new Investor(firstName, surname, username, password, companiesInvested, companiesInterested);
+		Investor john = new Investor(username, password, dob, firstName,
+				surname, email, telephone, address, companiesInvested,
+				companiesInterested);
+		assertEquals(username, john.getUsername());
+		assertEquals(password, john.getPassword());
+		assertEquals(dob, john.getDob());
 		assertEquals(firstName, john.getFirstName());
 		assertEquals(surname, john.getSurname());
+		assertEquals(email, john.getEmail());
+		assertEquals(telephone, john.getTelephone());
+		assertEquals(address, john.getAddress());
 	}
-	
+
+	@Test
+	public void DOBTest() {
+		Investor john = new Investor(username, password, dob, firstName,
+				surname, email, telephone, address, companiesInvested,
+				companiesInterested);
+		DOB birthday = john.getDob();
+		assertEquals(birthday.getBirthday()[0], 23);
+		assertEquals(birthday.getBirthday()[1], 11);
+		assertEquals(birthday.getBirthday()[2], 1950);
+	}
+
 	@Test
 	public void arrayListTest() {
-		Investor john = new Investor(firstName, surname, username, password, companiesInvested, companiesInterested);
-		
+		Investor john = new Investor(username, password, firstName, surname,
+				companiesInvested, companiesInterested);
+
 		assertTrue(john.isInterested("HSBA"));
 		assertTrue(john.isInvested("BARC"));
-		
+
 		assertFalse(john.isInterested("BARC"));
 		assertFalse(john.isInvested("HSBA"));
 	}
-	
+
 	@Test
 	public void addInterestedTest() {
-		Investor john = new Investor(firstName, surname, username, password, companiesInvested, companiesInterested);
-		
+		Investor john = new Investor(username, password, firstName, surname,
+				companiesInvested, companiesInterested);
+
 		assertTrue(john.isInterested("HSBA"));
 		assertTrue(john.isInvested("BARC"));
-		
+
 		john.addInterested("BARC");
-		
+
 		assertTrue(john.isInterested("BARC"));
 		assertFalse(john.isInvested("HSBA"));
 	}
-	
+
 	@Test
 	// create a profile with empty list
 	public void createClass() {
-		Investor john = new Investor(firstName, surname, username, password);
-		
+		Investor john = new Investor(username, password, firstName, surname);
+
 		assertFalse(john.isInterested("BARC"));
 		assertFalse(john.isInvested("BARC"));
 		assertFalse(john.isInterested("HSBA"));
 		assertFalse(john.isInvested("HSBA"));
-		
+
 		john.addInterested("BARC");
 		assertTrue(john.isInterested("BARC"));
 	}
