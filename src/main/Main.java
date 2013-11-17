9bb6f71c7a5689dcd5a714ac1c814f1bc412a94c
calum0607 <calum0607@hotmail.co.uk>
2013-11-17 23:00:45 +0000

    update vis of shares

--- src/main/Main.java
+++ src/main/Main.java
@@ -23,59 +23,77 @@ public class Main {
 			e.printStackTrace();
 		}
 		
-		LocalDate fromDate = new LocalDate(2012,11,17);
-
+		LocalDate fromDate = new LocalDate(2010,11,17);
+		
 		LocalDate toDate = new LocalDate();
 		System.out.println(toDate.getYear());
 		
 		LinkedList<Stock> ll = FinanceQuery.getHistorical("BP", fromDate, toDate, "d");
+		
+		System.out.println(ll.getFirst().getId());
 
 		Iterator<Stock> iterator = ll.iterator();
 		
 		
 		PrintWriter write = new PrintWriter("Data.js");
 
-		String jsonData = "var jsonData = [";
-		String priceData = "var priceData = [";
-		String volumeData = "var volumeData = [";
-		String summaryData = "var summaryData = [";
+		StringBuilder jsonData = new StringBuilder("");
+		StringBuilder priceData = new StringBuilder("");
+		StringBuilder volumeData = new StringBuilder("");
+		StringBuilder summaryData = new StringBuilder("");
 		int i=0;
 
 		while(iterator.hasNext()){
+			String jD = "";
+			String pD = "";
+			String vD = "";
+			String sD = "";			
+			Stock s = iterator.next();
+			
+			jD+= "{date:'"+str[s.getDate().getMonthOfYear()-1]+" "+s.getDate().getDayOfMonth()+", "+s.getDate().getYear()+"',";
+			jD+= "open:"+s.getOpen()+",";
+			jD+= "high:"+s.getHigh()+",";
+			jD+= "low:"+s.getLow()+",";
+			jD+= "close:"+s.getClose()+",";
+			jD+= "volume:"+s.getVolume()+"}";
+			//-----------------------------
+			pD+= "["+(ll.size()-1-i)+","+s.getClose()+"]";
+			//-----------------------------
+			vD+= "["+(ll.size()-1-i)+","+s.getVolume()+"]";
+			//-----------------------------
+			if(i%14==0)
+				sD+= "["+(ll.size()-1-i)+","+s.getClose()+"]";
+			
+			
+			
 			if(i!=0){
-				jsonData+= ",";
-				priceData+= ",";
-				volumeData+= ",";
+				jD+= ",";
+				pD+= ",";
+				vD+= ",";
 				
 			}
 			if(i%14==1){
-				summaryData+= ",";
+				sD+= ",";
 			}
-			
-			Stock s = iterator.next();
-			
-			jsonData+= "{date:'"+str[s.getDate().getMonthOfYear()-1]+" "+s.getDate().getDayOfMonth()+", "+s.getDate().getYear()+"',";
-			jsonData+= "open:"+s.getOpen()+",";
-			jsonData+= "high:"+s.getHigh()+",";
-			jsonData+= "low:"+s.getLow()+",";
-			jsonData+= "close:"+s.getClose()+",";
-			jsonData+= "volume:"+s.getVolume()+"}";
-			//-----------------------------
-			priceData+= "["+i+","+s.getClose()+"]";
-			//-----------------------------
-			volumeData+= "["+i+","+s.getVolume()+"]";
-			//-----------------------------
-			if(i%14==0)
-				summaryData+= "["+i+","+s.getClose()+"]";
-			
 			i++;
+			jsonData.insert(0,jD);
+			priceData.insert(0, pD);
+			volumeData.insert(0, vD);
+			summaryData.insert(0, sD);
 		}
+		jsonData.insert(0,"var jsonData = [");
+		priceData.insert(0, "var priceData = [");
+		volumeData.insert(0, "var volumeData = [");
+		summaryData.insert(0, "var summaryData = [");
+
 		write.println(jsonData+"];");
 		write.println(priceData+"];");
 		write.println(volumeData+"];");
 		write.println(summaryData+"];");
-
+		
 		write.close();
+		
+		System.out.println(ll.size());
 	}
 
 }
