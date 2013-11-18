e1e47c0a63f2ca3d82f0a64e128e0975fe259297
Jia Heng <jhe3@st-andrews.ac.uk>
2013-11-18 17:25:47 +0000

    Change value of FTSE100 constant

--- src/main/Constants.java
+++ src/main/Constants.java
@@ -2,7 +2,7 @@ package main;
 
 public final class Constants {
 	
-	public static final String FTSE100 = "@^FTSE";
+	public static final String FTSE100 = "^FTSE";
 	public static final String DAILY_INTERVAL = "d";
 	public static final String WEEKLY_INTERVAL = "w";
 	public static final String MONTHLY_INTERVAL = "m";
