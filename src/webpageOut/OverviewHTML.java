c4cebc01e0196a1ab70dfd38bafb3fd4bd65d3fe
CWKenny <cwk4@st-andrews.ac.uk>
2013-11-19 17:30:13 +0000

    MUCH faster Market Overview

--- src/webpageOut/OverviewHTML.java
+++ src/webpageOut/OverviewHTML.java
@@ -14,13 +14,11 @@ import main.Stock;
 
 //Writes HTML for Market Overview
 public class OverviewHTML extends WriteOut {
-	
-	
-	
-	public OverviewHTML(PrintWriter out, Investor ip){
-		this.out=out;
-		//Set title of HTML output
-		this.title="Market Overview";
+
+	public OverviewHTML(PrintWriter out, Investor ip) {
+		this.out = out;
+		// Set title of HTML output
+		this.title = "Market Overview";
 		writeOverview(ip);
 	}
 
@@ -46,50 +44,47 @@ public class OverviewHTML extends WriteOut {
 		out.println("<th>Market Cap</th>");
 		out.println("</tr>");
 	}
-	
+
 	private void writeStocks(Investor ip) {
-		NavigableSet<String> ftseList = FinanceQuery.getComponentsList(Constants.FTSE100);
-		Iterator<String> it = ftseList.iterator();
-		
-		LocalDate today = new LocalDate();
-		LocalDate yesterday = new LocalDate();
-		yesterday = yesterday.withDayOfMonth((yesterday.getDayOfMonth()-1));
-		
-		String sym;
+		String ftse = FinanceQuery.getComponents(Constants.FTSE100);
+		LinkedList<Stock> list = FinanceQuery.getLatestPrice(ftse);
+
+		Iterator<Stock> it = list.iterator();
+
 		Stock stock;
-		while(it.hasNext()){
-			sym = it.next();
-			LinkedList<Stock> list = FinanceQuery.getLatestPrice(sym);
-			stock = list.get(0);
-			//% Change of Current from Opening
-			double percent = (stock.getLatest()-stock.getOpen())/stock.getOpen();
-			percent = Math.floor(100000*percent+0.5)/1000;
-			//Assumes positive, if negative - sets to red
+		while (it.hasNext()) {
+			stock = it.next();
+
+			// % Change of Current from Opening
+			double percent = (stock.getLatest() - stock.getOpen())
+					/ stock.getOpen();
+			percent = Math.floor(100000 * percent + 0.5) / 1000;
+			// Assumes positive, if negative - sets to red
 			String change = "style=\"background-color:green\"";
-			if(stock.getLatest()<stock.getOpen()){
+			if (stock.getLatest() < stock.getOpen()) {
 				change = "style=\"background-color:red\"";
 			}
-			//Highlight share if user is interested/invested
+			// Highlight share if user is interested/invested
 			String interested = "";
-			if(ip.isInterested(sym)||ip.isInvested(sym)){
+			if (ip.isInterested(stock.getName())
+					|| ip.isInvested(stock.getName())) {
 				interested = "style=\"background-color:#71C6E2\"";
 			}
-			
-			//Write Table values
-			out.println("<tr "+interested+">");
-			out.println("<td "+interested+">"+stock.getId()+"</td>");
-			out.println("<td>"+stock.getName()+"</td>");
-			out.println("<td>"+stock.getLatest()+"</td>");
-			out.println("<td>"+stock.getOpen()+"</td>");
-			out.println("<td "+change+">"+percent+"%</td>");
-			out.println("<td>"+stock.getHigh()+"</td>");
-			out.println("<td>"+stock.getLow()+"</td>");
-			out.println("<td>"+stock.getVolume()+"</td>");
-			out.println("<td>"+stock.getMarketCap()+"</td>");
+
+			// Write Table values
+			out.println("<tr " + interested + ">");
+			out.println("<td " + interested + ">" + stock.getId() + "</td>");
+			out.println("<td>" + stock.getName() + "</td>");
+			out.println("<td>" + stock.getLatest() + "</td>");
+			out.println("<td>" + stock.getOpen() + "</td>");
+			out.println("<td " + change + ">" + percent + "%</td>");
+			out.println("<td>" + stock.getHigh() + "</td>");
+			out.println("<td>" + stock.getLow() + "</td>");
+			out.println("<td>" + stock.getVolume() + "</td>");
+			out.println("<td>" + stock.getMarketCap() + "</td>");
 			out.println("</tr>");
 		}
-		
+
 		out.println("</table>");
 	}
-
 }
