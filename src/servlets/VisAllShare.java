e0376c82081a43787123d6e6d606ac2fb50d201b
calum0607 <calum0607@hotmail.co.uk>
2013-11-23 18:59:35 +0000

    Create VisAllShare.java

---
+++ src/servlets/VisAllShare.java
@@ -0,0 +1,49 @@
+package servlets;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import main.Investor;
+import webpageOut.OverviewHTML;
+
+public class MarketOverview  extends HttpServlet {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -7429721582887832078L;
+	PrintWriter out;
+	
+	protected void doGet(HttpServletRequest servlet_request,
+			HttpServletResponse servlet_response) throws ServletException,
+			IOException {
+		processRequest(servlet_request, servlet_response);
+	}
+	
+	private void processRequest(HttpServletRequest servlet_request,
+			HttpServletResponse servlet_response) throws IOException {
+		
+		HttpSession session = servlet_request.getSession(false);
+		
+		if(session!=null && session.getAttribute("user")!=null){
+			servlet_response.setContentType("text/html"); //the response will be of the type html
+			servlet_response.setStatus(HttpServletResponse.SC_OK); //and the HTTP response code
+	
+			out = servlet_response.getWriter(); //creates writer
+			//used to send the html page to the client
+			
+			Investor investor = (Investor) session.getAttribute("user");
+			OverviewHTML o = new OverviewHTML(out, investor); //Write HTML
+			out.close();
+		} else {
+			servlet_response.sendRedirect("/static/HomePage.html");	
+		}
+	}
+
+}
