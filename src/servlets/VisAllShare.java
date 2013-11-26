571556692ddf7537e3f3a5f55c0423e8f2abbe04
calum0607 <calum0607@hotmail.co.uk>
2013-11-26 23:01:59 +0000

    final changes

--- src/servlets/VisAllShare.java
+++ src/servlets/VisAllShare.java
@@ -1,6 +1,5 @@
 package servlets;
 
-
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.Iterator;
@@ -12,9 +11,6 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
-
-
-
 import webpageOut.WriteOut;
 import main.Constants;
 import main.FinanceQuery;
@@ -25,140 +21,159 @@ import main.Stock;
  * Used to visualise all the shares that are part of an index
  * HTML and associated javascript code is based on: http://mbostock.github.io/protovis
  */
-public class VisAllShare extends HttpServlet{
+/**
+ * @author calumlaing
+ * 
+ */
+public class VisAllShare extends HttpServlet {
+
+	PrintWriter out; // will be used to write the response to the user
 
 	private static final long serialVersionUID = 5878729264427584184L;
 
-	/* (non-Javadoc)
-	 * @see javax.servlet.http.HttpServlet#doGet(javax.servlet.http.HttpServletRequest, javax.servlet.http.HttpServletResponse)
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see
+	 * javax.servlet.http.HttpServlet#doGet(javax.servlet.http.HttpServletRequest
+	 * , javax.servlet.http.HttpServletResponse)
 	 */
 	protected void doGet(HttpServletRequest servlet_request,
 			HttpServletResponse servlet_response) throws ServletException,
 			IOException {
 		processRequest(servlet_request, servlet_response);
 	}
-	
+
 	/**
-	 * @param servlet_request The index (e.g. FTSE100) which is to be visualised
-	 * @param servlet_response The visualisation of the data
+	 * @param servlet_request
+	 *            The index (e.g. FTSE100) which is to be visualised
+	 * @param servlet_response
+	 *            The visualisation of the data
 	 * @throws IOException
 	 */
 	private void processRequest(HttpServletRequest servlet_request,
 			HttpServletResponse servlet_response) throws IOException {
 
 		HttpSession session = servlet_request.getSession(false);
-		
-		//checks an investor is signed into the system
-		if(session==null || session.getAttribute("user")==null){
+
+		// checks an investor is signed into the system
+		if (session == null || session.getAttribute("user") == null) {
 			servlet_response.sendRedirect("/static/HomePage.html");
 			return;
 		}
-		
-		servlet_response.setContentType("text/html"); //the response will be of the type html
-		servlet_response.setStatus(HttpServletResponse.SC_OK); //and the HTTP response code
 
-		PrintWriter out = servlet_response.getWriter(); //creates writer
-		//used to send the html page to the client
-		
-		//parameter to determine if the the ftse100 or the nasdaq100 are to be displayed
+		servlet_response.setContentType("text/html"); // the response will be of
+														// the type html
+		servlet_response.setStatus(HttpServletResponse.SC_OK); // and the HTTP
+																// response code
+
+		PrintWriter out = servlet_response.getWriter(); // creates writer
+		// used to send the html page to the client
+
+		// parameter to determine if the the ftse100 or the nasdaq100 are to be
+		// displayed
 		String sExchange = servlet_request.getParameter("exchange");
-		String index="";
+		String index = "";
 
-		if(sExchange.equals(Constants.FTSE100)){
-			index="FTSE 100";
-		}
-		else if(sExchange.equals(Constants.NASDAQ100)){
-			index="NASDAQ 100";
-		}
-		else{
+		if (sExchange.equals(Constants.FTSE100)) {
+			index = "FTSE 100";
+		} else if (sExchange.equals(Constants.NASDAQ100)) {
+			index = "NASDAQ 100";
+		} else {
 			return;
 		}
-			
-		//get all the shares in an index
+
+		// get all the shares in an index
 		String ex = FinanceQuery.getComponents(sExchange);
-		//get all the shares in an index returned as a LinkedList
+		// get all the shares in an index returned as a LinkedList
 		LinkedList<Stock> list = FinanceQuery.getLatestPrice(ex);
 
+		printOutWebpage(list, index);
+
+	}
+
+	/**
+	 * Used to print out the graph of all the shares belonging to a certain
+	 * index
+	 * 
+	 * @param list
+	 *            list of all the shares in the index
+	 * @param index
+	 *            the index to visualise, e.g. FTSE100
+	 */
+	public void printOutWebpage(LinkedList<Stock> list, String index) {
 		Iterator<Stock> it = list.iterator();
 
 		WriteOut w = new WriteOut();
 		w.setWriter(out);
-		
+
 		out.println("<!DOCTYPE html>");
 		out.println("<html>");
-		  out.println("<head>");
-		    out.println("<script src=\"../static/javascript/VisAllShare/protovis.js\" type=\"text/javascript\"></script>");
-		    out.println("<script src=\"../static/javascript/VisAllShare/jquery-1.4.2.min.js\" type=\"text/javascript\"></script>");
-		    out.println("<script src=\"../static/javascript/VisAllShare/jquery.tipsy.js\" type=\"text/javascript\"></script>");
-		    out.println("<script src=\"../static/javascript/VisAllShare/tipsy.js\" type=\"text/javascript\"></script>");
-		    out.println("<link href=\"../static/javascript/VisAllShare/tipsy.css\" type=\"text/css\" rel=\"stylesheet\"/>");
-		    out.println("<title>All Share Visualisation</title>");
-		    out.println("<style type=\"text/css\">");
-
+		out.println("<head>");
+		// all the javascript files that are needed to visualise the shares
+		out.println("<script src=\"../static/javascript/VisAllShare/protovis.js\" type=\"text/javascript\"></script>");
+		out.println("<script src=\"../static/javascript/VisAllShare/jquery-1.4.2.min.js\" type=\"text/javascript\"></script>");
+		out.println("<script src=\"../static/javascript/VisAllShare/jquery.tipsy.js\" type=\"text/javascript\"></script>");
+		out.println("<script src=\"../static/javascript/VisAllShare/tipsy.js\" type=\"text/javascript\"></script>");
+		out.println("<link href=\"../static/javascript/VisAllShare/tipsy.css\" type=\"text/css\" rel=\"stylesheet\"/>");
+		out.println("<title>All Share Visualisation</title>");
+		out.println("<style type=\"text/css\">");
 		out.println("body {");
-		  out.println("font: 10px sans-serif;");
+		out.println("font: 10px sans-serif;");
 		out.println("}");
 
-	    out.println("</style>");
-	  out.println("</head>");
-	  out.println("<body>");
-	   w.writeHeader(); //Write Header
-	   out.println("<h1>Visualisation of shares in "+index+"</h1>");
-	  out.println("<script type=\"text/javascript+protovis\">");
-	    out.println("var data = [");
-	    
+		out.println("</style>");
+		out.println("</head>");
+		out.println("<body>");
+		w.writeHeader(); // Write the webpage header
+		out.println("<h1>Visualisation of shares in " + index + "</h1>");
+		out.println("<script type=\"text/javascript+protovis\">");
+		out.println("var data = [");
 		Stock stock;
-		String stockID="";
-		int i=0;
-		double max=0;
+		String stockID = "";
+		int i = 0;
+		double max = 0;
+		// go through the list of stocks and add the latest price of each stock
+		// the string (javascript array)
 		while (it.hasNext()) {
 			stock = it.next();
-			if(i>0){
+			if (i > 0) {
 				out.print(",");
-				stockID+=",";
-				if(stock.getLatest()>max){
-					max=stock.getLatest();
+				stockID += ",";
+				if (stock.getLatest() > max) {
+					max = stock.getLatest();
 				}
-			}
-			else{
-				max=stock.getLatest();
+			} else {
+				max = stock.getLatest();
 			}
 			i++;
-			
 			out.print(stock.getLatest());
-			stockID=stockID+"\""+stock.getId()+"\"";
+			stockID = stockID + "\"" + stock.getId() + "\"";
 		}
-		
-		int a = (int)max/100;
-		
-		System.out.println(a);
-		
+		// used to the range of the graph.
+		// will be used round the value to the nearest 100
+		// see below: (a+1)*100
+		// i.e. if max is 1567, the group will range from 0 to 1600
+		int a = (int) max / 100;
+
 		out.print("],");
-		out.println("share = ["+stockID+"],");
+		out.println("share = [" + stockID + "],");
 		out.println("w =1200,");
 		out.println("h = 2000,");
-		out.print("x = pv.Scale.linear(0,"+((a+1)*100)+").range(0, w),");
-		out.println("y = pv.Scale.ordinal(pv.range("+list.size()+")).splitBanded(0, h, 4/5);");
-		
+		out.print("x = pv.Scale.linear(0," + ((a + 1) * 100) + ").range(0, w),");
+		out.println("y = pv.Scale.ordinal(pv.range(" + list.size()
+				+ ")).splitBanded(0, h, 4/5);");
+		// used to format the output graph
 		out.println("var vis = new pv.Panel().width(w).height(h).bottom(20).left(50).right(10).top(5);");
-
 		out.println("var bar = vis.add(pv.Bar).data(data).top(function() y(this.index)).height(y.range().band).left(0).width(x).title(function(d) d.toFixed(2)).event(\"mouseover\", pv.Behavior.tipsy({gravity: \"w\", fade: true}));");
-
 		out.println("bar.anchor(\"left\").add(pv.Label).textMargin(10).textAlign(\"right\").text(function() share[this.index]);");
-
 		out.println("vis.add(pv.Rule).data(x.ticks()).left(function(d) Math.round(x(d)) - .5).strokeStyle(function(d) d ? \"rgba(0,0,0,.3)\" : \"#000\").add(pv.Rule).bottom(0).height(5).strokeStyle(\"#000\").anchor(\"bottom\").add(pv.Label).text(function(d) d.toFixed(1));");
-
 		out.println("vis.render();");
 
-		    out.println("</script>");
-		    out.println("<div id=\"example\"></div>");
-		  out.println("</body>");
+		out.println("</script>");
+		out.println("<div id=\"example\"></div>");
+		out.println("</body>");
 		out.println("</html>");
-		
 	}
-	
-	public void printOutWebpage(){
-		
-	}
-	
+
 }
--- src/servlets/VisShare.java
+++ src/servlets/VisShare.java
@@ -21,12 +21,10 @@ import org.joda.time.LocalDate;
 import webpageOut.WriteOut;
 
 /**
- * @author cl72
- * Used to visualise a single share
- * HTML and associated javascript code is based on: http://www.humblesoftware.com/envision
+ * @author cl72 Used to visualise a single share HTML and associated javascript
+ *         code is based on: http://www.humblesoftware.com/envision
  */
-public class VisShare extends HttpServlet{
-			
+public class VisShare extends HttpServlet {
 
 	private static final long serialVersionUID = 934297545825976921L;
 
@@ -35,36 +33,38 @@ public class VisShare extends HttpServlet{
 			IOException {
 		processRequest(servlet_request, servlet_response);
 	}
-	
+
 	private void processRequest(HttpServletRequest servlet_request,
 			HttpServletResponse servlet_response) throws IOException {
 
 		HttpSession session = servlet_request.getSession(false);
-		//return to homepage if not sign in
-		if(session==null || session.getAttribute("user")==null){
+		// return to homepage if not sign in
+		if (session == null || session.getAttribute("user") == null) {
 			servlet_response.sendRedirect("/static/HomePage.html");
 			return;
 		}
-		
-		servlet_response.setContentType("text/html"); //the response will be of the type html
-		servlet_response.setStatus(HttpServletResponse.SC_OK); //and the HTTP response code
 
-		//gets the id of the share which has to be displayed
+		servlet_response.setContentType("text/html"); // the response will be of
+														// the type html
+		servlet_response.setStatus(HttpServletResponse.SC_OK); // and the HTTP
+																// response code
+
+		// gets the id of the share which has to be displayed
 		String id = servlet_request.getParameter("id");
-		
+
 		writeDataFile(id);
-		
-		PrintWriter out = servlet_response.getWriter(); //creates writer
-		//used to send the html page to the client
-		
+
+		PrintWriter out = servlet_response.getWriter(); // creates writer
+		// used to send the html page to the client
+
 		WriteOut w = new WriteOut();
 		w.setWriter(out);
-		
+
 		out.println("<!DOCTYPE html PUBLIC \"-//W3C//DTD XHTML 1.0 Strict//EN\" \"http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd\">");
 		out.println("<html xmlns=\"http://www.w3.org/1999/xhtml\" xml:lang=\"en\">");
 		out.println("<head>");
 		out.println("<meta charset=\"utf-8\">");
-		out.println("<title> Share Data - "+id+ "</title>");
+		out.println("<title> Share Data - " + id + "</title>");
 		out.println("<link rel=\"stylesheet\" href=\"../static/javascript/Vis_Files/finance.css\" type=\"text/css\" />");
 		out.println("<script type=\"text/javascript\" src=\"../static/javascript/Vis_Files/prototype.js\"></script>");
 		out.println("<script type=\"text/javascript\" src=\"../static/javascript/Vis_Files/scriptaculous.js\"></script>");
@@ -74,108 +74,117 @@ public class VisShare extends HttpServlet{
 		out.println("<script type=\"text/javascript\" src=\"../static/javascript/Vis_Files/canvastext.js\"></script>");
 		out.println("<script type=\"text/javascript\" src=\"../static/javascript/Vis_Files/flotr.js\"></script>");
 		out.println("<script type=\"text/javascript\" src=\"../static/javascript/Vis_Files/HumbleFinance.js\"></script>");
-		out.println("<script type=\"text/javascript\" src=\"../static/javascript/Vis_Files/data-"+id+".js\"></script>");	
-		
+		out.println("<script type=\"text/javascript\" src=\"../static/javascript/Vis_Files/data-"
+				+ id + ".js\"></script>");
+
 		out.println("<script type=\"text/javascript\" src=\"../static/javascript/Vis_Files/demo.js\"></script>");
 		out.println("</head>");
 		out.println("<body>");
-		
+
 		w.writeHeader();
-		
+
 		out.println("<div id=\"body-container\" >");
 		out.println("<div id=\"header-container\"><div id=\"header\"></div></div>");
 		out.println("<div id=\"links-container\"><div id=\"links\"></div></div>");
 		out.println("<div id=\"content-container\"><div id=\"content\">");
 		out.println("<body>");
-		out.println("<h3>Share Data - "+id+"</h3>");
+		out.println("<h3>Share Data - " + id + "</h3>");
 		out.println("<div id=\"humblefinance\" style=\"position: relative; margin: 40px 0px; width: 600px; border: 1px solid #99CCFF;\";></div>");
 		out.println("</div></div>");
 		out.println("<div id=\"footer-container\"><div id=\"footer\"></div></div>");
 		out.println("</div>");
 		out.println("</body>");
 		out.println("</html>");
-		out.close(); 
-		}
-	
-	
+		out.close();
+	}
+
 	/**
-	 * Creates the javascript file that contains all the data about the share that will be visualised
-	 * @param stock the id of the stock that will be displayed
+	 * Creates the javascript file that contains all the data about the share
+	 * that will be visualised
+	 * 
+	 * @param stock
+	 *            the id of the stock that will be displayed
 	 * 
 	 */
-	public void writeDataFile(String stock) throws FileNotFoundException{
+	public void writeDataFile(String stock) throws FileNotFoundException {
 
 		StringBuilder jsonData = new StringBuilder("");
 		StringBuilder priceData = new StringBuilder("");
 		StringBuilder volumeData = new StringBuilder("");
 		StringBuilder summaryData = new StringBuilder("");
-		
-		String[] str = {"January", "February", "March", "April","May","June", "July","August", "September","October","November", "December"};
-				
-		LocalDate fromDate = new LocalDate(2010,11,17);
-		
+
+		String[] str = { "January", "February", "March", "April", "May",
+				"June", "July", "August", "September", "October", "November",
+				"December" };
+
+		LocalDate fromDate = new LocalDate(2010, 11, 17);
+
 		LocalDate toDate = new LocalDate();
-		
-		LinkedList<Stock> ll = FinanceQuery.getHistorical(stock, fromDate, toDate, "d");
+
+		LinkedList<Stock> ll = FinanceQuery.getHistorical(stock, fromDate,
+				toDate, "d");
 
 		Iterator<Stock> iterator = ll.iterator();
-				
-		File file = new File("./WebRoot/static/javascript/Vis_Files/data-"+stock+".js");
+
+		File file = new File("./WebRoot/static/javascript/Vis_Files/data-"
+				+ stock + ".js");
 		file.deleteOnExit();
 		PrintWriter write = new PrintWriter(file);
-		
-		int i=0;
 
-		while(iterator.hasNext()){
+		int i = 0;
+
+		while (iterator.hasNext()) {
 			String jD = "";
 			String pD = "";
 			String vD = "";
-			String sD = "";			
+			String sD = "";
 			Stock s = iterator.next();
-			
-			//will contain all the key points about the share
-			jD+= "{date:'"+str[s.getDate().getMonthOfYear()-1]+" "+s.getDate().getDayOfMonth()+", "+s.getDate().getYear()+"',";
-			jD+= "open:"+s.getOpen()+",";
-			jD+= "high:"+s.getHigh()+",";
-			jD+= "low:"+s.getLow()+",";
-			jD+= "close:"+s.getClose()+",";
-			jD+= "volume:"+s.getVolume()+"}";
-			//-----------------------------
-			//will store the closing price of the share over the range of time
-			pD+= "["+(ll.size()-1-i)+","+s.getClose()+"]";
-			//-----------------------------
-			//will store the volume sold of the share over the range in time
-			vD+= "["+(ll.size()-1-i)+","+s.getVolume()+"]";
-			//-----------------------------
-			//summary of the data, same as price data
-			sD+= "["+(ll.size()-1-i)+","+s.getClose()+"]";
-			
-			if(i!=0){
-				jD+= ",";
-				pD+= ",";
-				vD+= ",";
-				sD+=",";
+
+			// will contain all the key points about the share
+			jD += "{date:'" + str[s.getDate().getMonthOfYear() - 1] + " "
+					+ s.getDate().getDayOfMonth() + ", "
+					+ s.getDate().getYear() + "',";
+			jD += "open:" + s.getOpen() + ",";
+			jD += "high:" + s.getHigh() + ",";
+			jD += "low:" + s.getLow() + ",";
+			jD += "close:" + s.getClose() + ",";
+			jD += "volume:" + s.getVolume() + "}";
+			// -----------------------------
+			// will store the closing price of the share over the range of time
+			pD += "[" + (ll.size() - 1 - i) + "," + s.getClose() + "]";
+			// -----------------------------
+			// will store the volume sold of the share over the range in time
+			vD += "[" + (ll.size() - 1 - i) + "," + s.getVolume() + "]";
+			// -----------------------------
+			// summary of the data, same as price data
+			sD += "[" + (ll.size() - 1 - i) + "," + s.getClose() + "]";
+
+			if (i != 0) {
+				jD += ",";
+				pD += ",";
+				vD += ",";
+				sD += ",";
 			}
 
 			i++;
-			//add to the front of the string
-			//[so that olds dates are first and newest dates are last]
-			//when iterating over the list - new dates are done first
-			jsonData.insert(0,jD);
+			// add to the front of the string
+			// [so that olds dates are first and newest dates are last]
+			// when iterating over the list - new dates are done first
+			jsonData.insert(0, jD);
 			priceData.insert(0, pD);
 			volumeData.insert(0, vD);
 			summaryData.insert(0, sD);
 		}
-		jsonData.insert(0,"var jsonData = [");
+		jsonData.insert(0, "var jsonData = [");
 		priceData.insert(0, "var priceData = [");
 		volumeData.insert(0, "var volumeData = [");
 		summaryData.insert(0, "var summaryData = [");
 
-		write.println(jsonData+"];");
-		write.println(priceData+"];");
-		write.println(volumeData+"];");
-		write.println(summaryData+"];");
-		
+		write.println(jsonData + "];");
+		write.println(priceData + "];");
+		write.println(volumeData + "];");
+		write.println(summaryData + "];");
+
 		write.close();
 	}
 }
