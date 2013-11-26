8a9109a14eecec8648f7f8437b876ad385e71050
calum0607 <calum0607@hotmail.co.uk>
2013-11-26 22:12:17 +0000

    UPDATE

--- src/servlets/SignOut.java
+++ src/servlets/SignOut.java
@@ -8,21 +8,34 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
+/**
+ * Used to sign the investor out of the application
+ * 
+ */
 public class SignOut extends HttpServlet {
 
 	private static final long serialVersionUID = -2403528070614551707L;
 
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see
+	 * javax.servlet.http.HttpServlet#doGet(javax.servlet.http.HttpServletRequest
+	 * , javax.servlet.http.HttpServletResponse)
+	 */
 	protected void doGet(HttpServletRequest servlet_request,
 			HttpServletResponse servlet_response) throws ServletException,
 			IOException {
 		processRequest(servlet_request, servlet_response);
 	}
-	
+
 	private void processRequest(HttpServletRequest servlet_request,
 			HttpServletResponse servlet_response) throws IOException {
 		HttpSession session = servlet_request.getSession(false);
-		
+
+		// checks if there is currently a session running
 		if (session != null) {
+			// if there is - the session will be ended
 			session.invalidate();
 		}
 		servlet_response.sendRedirect("/static/HomePage.html");
--- src/servlets/StockChooser.java
+++ src/servlets/StockChooser.java
@@ -11,35 +11,42 @@ import javax.servlet.http.HttpSession;
 
 import webpageOut.StockListHTML;
 
-//Generates List of Stocks currently in FTSE100 which a user can select to visualise
+/**
+ * Generates List of Stock currently in FTSE100 and NASDAQ100 which a user can select to visualise
+ */
 public class StockChooser extends HttpServlet {
 
 	private static final long serialVersionUID = -3372993162487229776L;
-PrintWriter out;
-	
+	PrintWriter out;
+
 	protected void doGet(HttpServletRequest servlet_request,
 			HttpServletResponse servlet_response) throws ServletException,
 			IOException {
 		processRequest(servlet_request, servlet_response);
 	}
-	
+
 	private void processRequest(HttpServletRequest servlet_request,
 			HttpServletResponse servlet_response) throws IOException {
-		
+
 		HttpSession session = servlet_request.getSession(false);
-		
-		if(session!=null && session.getAttribute("user")!=null){
-			servlet_response.setContentType("text/html"); //the response will be of the type html
-			servlet_response.setStatus(HttpServletResponse.SC_OK); //and the HTTP response code
-	
-			out = servlet_response.getWriter(); //creates writer
-			//used to send the html page to the client
-			
-			StockListHTML s = new StockListHTML(out); //Write HTML
+
+		if (session != null && session.getAttribute("user") != null) {
+			servlet_response.setContentType("text/html"); // the response will
+															// be of the type
+															// html
+			servlet_response.setStatus(HttpServletResponse.SC_OK); // and the
+																	// HTTP
+																	// response
+																	// code
+
+			out = servlet_response.getWriter(); // creates writer
+			// used to send the html page to the client
+
+			StockListHTML s = new StockListHTML(out); // Write HTML
 			s.writeHTML();
 			out.close();
 		} else {
-			servlet_response.sendRedirect("/static/HomePage.html");	
+			servlet_response.sendRedirect("/static/HomePage.html");
 		}
 	}
 
